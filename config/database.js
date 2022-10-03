972700bf0ef3e8d6da2c8b434861556f2554ce97
miketabb33 <miketabb33@gmail.com>
2022-10-02 22:25:36 -0700

    Add prod db connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,9 @@
-module.exports = {
-  //mongoDbUrl: 'mongodb://127.0.0.1:27017/cms'
-  mongoDbUrl: 'mongodb+srv://udemy_cms:edQRMLgHpOlnr39J@cluster0.tnava24.mongodb.net/test'
+if(process.env.NODE_ENV == 'production') {
+  module.exports = {
+    mongoDbUrl: 'mongodb+srv://udemy_cms:edQRMLgHpOlnr39J@cluster0.tnava24.mongodb.net/test'
+  } 
+} else {
+  module.exports = {
+    mongoDbUrl: 'mongodb://127.0.0.1:27017/cms'
+  }
 }
